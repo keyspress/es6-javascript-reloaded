2b42b72c422c157868cc32acff0e55a03dada652
Kyle Desktop <kylespresser@gmail.com>
2019-01-10 23:02:39 -0500

    lecture 13

--- objects.js
+++ objects.js
@@ -11,16 +11,27 @@
 
 // console.log(user.firstname, user.lastname, user.fullName);
 
-let obj = {
-  x: 7,
-  y: 8,
-  z: 9
-};
+// let obj = {
+//   x: 7,
+//   y: 8,
+//   z: 9
+// };
 
 // let x = obj.x,
 //   y = obj.y,
 //   z = obj.z;
 
-let { x, y, z } = obj;
+// let { x, y, z } = obj;
 
-console.log(x, y, z);
+// console.log(x, y, z);
+
+let myObj = {
+  prop1: 'hello',
+  prop2: 'world',
+  fullName(firstname, lastname) {
+    let fullName = firstname + ' ' + lastname;
+    return fullName;
+  }
+};
+
+console.log(myObj.fullName('Homey', 'McComey'));
