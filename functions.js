9bcc2045360298cb87ab2716d79f733c6a9f1200
Kyle Desktop <kylespresser@gmail.com>
2019-01-10 22:32:44 -0500

    lecture 7

--- functions.js
+++ functions.js
@@ -8,8 +8,19 @@
 
 // box();
 
-function multiply(num = 2, numTwo = num) {
-  return num * numTwo;
+// function multiply(num = 2, numTwo = num) {
+//   return num * numTwo;
+// }
+
+// console.log(multiply(3, 10));
+function sum(...numbers) {
+  var result = 0;
+
+  numbers.forEach(function(number) {
+    result += number;
+  });
+
+  return result;
 }
 
-console.log(multiply(3, 10));
+console.log(sum(3, 5, 7, 9));
