cd4fc44f50766336fa170198cbd5de4def4dadfe
Kyle Desktop <kylespresser@gmail.com>
2019-01-10 22:57:43 -0500

    lecture 12

--- objects.js
+++ objects.js
@@ -1,12 +1,26 @@
-function createUser(firstname, lastname) {
-  let fullName = firstname + ' ' + lastname;
-  return {
-    firstname,
-    lastname,
-    fullName
-  };
-}
+// function createUser(firstname, lastname) {
+//   let fullName = firstname + ' ' + lastname;
+//   return {
+//     firstname,
+//     lastname,
+//     fullName
+//   };
+// }
 
-let user = createUser('Jim', 'Jones');
+// let user = createUser('Jim', 'Jones');
 
-console.log(user.firstname, user.lastname, user.fullName);
+// console.log(user.firstname, user.lastname, user.fullName);
+
+let obj = {
+  x: 7,
+  y: 8,
+  z: 9
+};
+
+// let x = obj.x,
+//   y = obj.y,
+//   z = obj.z;
+
+let { x, y, z } = obj;
+
+console.log(x, y, z);
