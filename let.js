6d701eca1a2ecb10f2c66d854ea4c71dc8c2c206
Kyle Desktop <kylespresser@gmail.com>
2019-01-10 22:05:22 -0500

    lecture 3

--- let.js
+++ let.js
@@ -1,12 +1,24 @@
-function showLength(array) {
-  if (array.length > 5) {
-    var greater = 'Greater than 5';
-    console.log(greater);
-  } else {
-    let lessThan = 'less than 5';
-    console.log(lessThan);
-    console.log(greater);
-  }
+// function showLength(array) {
+//   if (array.length > 5) {
+//     var greater = 'Greater than 5';
+//     console.log(greater);
+//   } else {
+//     let lessThan = 'less than 5';
+//     console.log(lessThan);
+//     console.log(greater);
+//   }
+// }
+
+// showLength([1, 2, 3]);
+
+var funcs = [];
+
+for (let i = 0; i < 5; i++) {
+  funcs.push(function() {
+    console.log(i);
+  });
 }
 
-showLength([1, 2, 3]);
+funcs[0]();
+funcs[1]();
+funcs[4]();
