f127ea8a770f3fbe26c35dbcf0078d072f9ee430
Kyle Desktop <kylespresser@gmail.com>
2019-01-16 22:50:25 -0500

    lecture 27

--- sets.js
+++ sets.js
@@ -13,11 +13,35 @@
 
 // console.log(a, b, c);
 
-let weakCars = new WeakSet();
+// let weakCars = new WeakSet();
 
-weakCars.add({ driver: 'Alf' });
-let passenger = { passenger: 'Sarah' };
-weakCars.add(passenger);
-weakCars.delete(passenger);
+// weakCars.add({ driver: 'Alf' });
+// let passenger = { passenger: 'Sarah' };
+// weakCars.add(passenger);
+// weakCars.delete(passenger);
 
-console.log(weakCars.has(passenger));
+// console.log(weakCars.has(passenger));
+
+let carSlides = [
+  { car: 'Audi', seen: false, image: 'url' },
+  { car: 'Ford', seen: false, image: 'url' },
+  { car: 'Mercedes', seen: false, image: 'url' },
+  { car: 'VW', seen: false, image: 'url' }
+];
+
+let carsViewed = new WeakSet();
+
+function clicked(carSlides) {
+  carSlides.forEach(car => {
+    carsViewed.add(car);
+  });
+}
+
+let linkClicked = true;
+if (linkClicked) {
+  clicked(carSlides);
+}
+
+for (let car of carSlides) {
+  console.log(carsViewed.has(car));
+}
