5e03cb95690f05cce1c689f9058defff02c2a5ff
Kyle Desktop <kylespresser@gmail.com>
2019-01-16 22:42:58 -0500

    lecture 26

--- sets.js
+++ sets.js
@@ -2,13 +2,22 @@
 
 // console.log(cars.length);
 
-let cars = new Set();
+// let cars = new Set();
 
-cars.add('Audi');
-cars.add('Ford');
-cars.add('Mercedes');
-cars.add('Audi');
+// cars.add('Audi');
+// cars.add('Ford');
+// cars.add('Mercedes');
+// cars.add('Audi');
 
-let [a, b, c] = cars;
+// let [a, b, c] = cars;
 
-console.log(a, b, c);
+// console.log(a, b, c);
+
+let weakCars = new WeakSet();
+
+weakCars.add({ driver: 'Alf' });
+let passenger = { passenger: 'Sarah' };
+weakCars.add(passenger);
+weakCars.delete(passenger);
+
+console.log(weakCars.has(passenger));
