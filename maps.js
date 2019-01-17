0e399bf3eea4b92846089246ddb293a0c79f91fa
Kyle Desktop <kylespresser@gmail.com>
2019-01-16 22:30:40 -0500

    lecture 22

--- maps.js
+++ maps.js
@@ -13,13 +13,23 @@
 
 // console.log(carAge);
 
-let cars = new Map();
+// let cars = new Map();
 
-cars.set('CarOne', 'Audi');
-cars.set('CarTwo', 'Ford');
-cars.set('CarThree', 'GM');
-cars.set('CarFour', 'BMW');
+// cars.set('CarOne', 'Audi');
+// cars.set('CarTwo', 'Ford');
+// cars.set('CarThree', 'GM');
+// cars.set('CarFour', 'BMW');
 
-for (let [key, value] of cars) {
-  console.log(`${key} = ${value}`);
-}
+// for (let [key, value] of cars) {
+//   console.log(`${key} = ${value}`);
+// }
+
+let person = {
+  name: 'Abdi',
+  address: '123 Js Street',
+  occupation: 'JS Dev'
+};
+
+let numbers = [1, 2, 3];
+
+console.log(typeof person[Symbol.iterator]);
