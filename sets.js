35f52c94ba933cb9e78172693439cb21f2f81202
Kyle Desktop <kylespresser@gmail.com>
2019-01-16 22:37:59 -0500

    lecture 24

---
+++ sets.js
@@ -0,0 +1,12 @@
+// let cars = ['Audi', 'Ford', 'Audi', 'Mercedes', 'VW'];
+
+// console.log(cars.length);
+
+let cars = new Set();
+
+cars.add('Audi');
+cars.add('Ford');
+cars.add('Mercedes');
+cars.add('Audi');
+
+console.log(cars.size);
